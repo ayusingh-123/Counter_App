e89b59c475f6f94b433736ba34a0e890df25a328
ayusingh-123 <83396369+ayusingh-123@users.noreply.github.com>
2023-01-31 15:21:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Segment,
+} from 'semantic-ui-react'
+import { useState } from 'react';
 
 function App() {
+  const[count,setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Segment style={{ padding: '15em 8em' }} vertical>
+    
+      <Container textAlign='center'>
+      <div className='card'>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Counter App
+        </Header>
+        <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column textAlign='center'>
+          <p style={{ fontSize: '1.33em' }}>
+        
+        <Container className="my-5">
+          
+            <div className='main'><Container textAlign='center'>{count}</Container></div>
+            <Button className="mx-2" color='green' onClick={()=>setCount(count+1)} >Increment </Button> &emsp; &ensp; &emsp;&emsp;
+            <Button className="mx-3" color='red' onClick={()=>setCount(count-1)} disabled={count===0}>Decrement </Button>
+   
+        </Container>
+</p>
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
+        </div>
+      </Container>
+      <Header textAlign='center'>Assignment done by Ayush Singh Parihar 12010440</Header>
+    </Segment>
+
+    </>
   );
 }
 
